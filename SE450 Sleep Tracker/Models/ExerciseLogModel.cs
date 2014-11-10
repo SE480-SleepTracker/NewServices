335fdec0b9c3a63dd72ecdfbbe00095d9dab0b9f
ethansudman@comcast.net <ethansudman@comcast.net>
2014-11-09 20:50:58 -0600

    Fill out model logic

--- SE450 Sleep Tracker/Models/ExerciseLogModel.cs
+++ SE450 Sleep Tracker/Models/ExerciseLogModel.cs
@@ -2,10 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SE450Database;
+using System.Configuration;
+using System;
 
 namespace SE450_Sleep_Tracker.Models
 {
     public class ExerciseLogModel
     {
+        public int ID { get; set; }
+
+        public string UserID { get; set; }
+
+        public ExerciseTypeModel Type { get; set; }
+
+        public ExerciseIntensityModel Intensity { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public ExerciseLogModel(Exr_Exercise exercise)
+        {
+            ID = exercise.Exr_ID;
+            UserID = exercise.Exr_aur_id;
+            
+            string connectionString = ConfigurationManager.ConnectionStrings["LinqConnection"].ConnectionString;
+            using (var db = new SleepMonitor(connectionString))
+            {
+                var its = db.Exi_ExerciseIntensity.FirstOrDefault(it => it.Exi_id == exercise.Exr_exi_ID);
+                Intensity = new ExerciseIntensityModel(its);
+
+                var tp = db.Ext_ExerciseTypes.FirstOrDefault(ty => ty.Ext_ID == exercise.Exr_ext_id);
+
+                Type = new ExerciseTypeModel
+                {
+                    CaloriesPerHour = tp.Ext_calories_per_hour,
+                    ID = tp.Ext_ID,
+                    Intensity = this.Intensity,
+                    Name = tp.Ext_name
+                };
+            }
+        }
+
+        public Exr_Exercise ToDbObject() 
+        {
+            return new Exr_Exercise
+            {
+                Exr_aur_id = UserID,
+                Exr_end = End,
+                Exr_exi_ID = Intensity.ID,
+                Exr_ext_id = Type.ID,
+                Exr_ID = ID,
+                Exr_start = Start,
+                Ext_ExerciseTypes = Type.ToDbObject(),
+                Exi_ExerciseIntensity = Intensity.ToDbObject()
+            };
+        }
     }
 }
--- SE450 Sleep Tracker/Models/ExerciseTypeModel.cs
+++ SE450 Sleep Tracker/Models/ExerciseTypeModel.cs
@@ -2,10 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SE450Database;
 
 namespace SE450_Sleep_Tracker.Models
 {
     public class ExerciseTypeModel
     {
+        public int ID { get; set; }
+
+        public ExerciseIntensityModel Intensity { get; set; }
+
+        public double CaloriesPerHour { get; set; }
+
+        public string Name { get; set; }
+
+        public ExerciseTypeModel() {}
+
+        public ExerciseTypeModel(Ext_ExerciseTypes type)
+        {
+            CaloriesPerHour = type.Ext_calories_per_hour;
+            ID = type.Ext_ID;
+            Name = type.Ext_name;
+
+            Exi_ExerciseIntensity its;
+            using (var db = DataCurator.GetConnection())
+            {
+                its = db.Exi_ExerciseIntensity.FirstOrDefault(i => i.Exi_id == type.Ext_exi_intensity);
+            }
+            Intensity = new ExerciseIntensityModel(its);
+        }
+
+        public Ext_ExerciseTypes ToDbObject()
+        {
+            return new Ext_ExerciseTypes
+            {
+                Ext_calories_per_hour = CaloriesPerHour,
+                Ext_exi_intensity = Intensity.ID,
+                Ext_ID = ID,
+                Ext_name = Name
+            };
+        }
     }
 }
