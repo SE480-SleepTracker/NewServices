183a060bf0fb6c56d5a25e7e657f3a3e731cace7
ethansudman@comcast.net <ethansudman@comcast.net>
2014-11-09 20:50:19 -0600

    Move boilerplate out to its own class

---
+++ SE450 Sleep Tracker/Models/DataCurator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SE450Database;
+using System.Configuration;
+
+namespace SE450_Sleep_Tracker.Models
+{
+    public class DataCurator
+    {
+        private static readonly string connectionString;
+
+        static DataCurator()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["LinqConnection"].ConnectionString;
+        }
+
+        public static SleepMonitor GetConnection()
+        {
+            return new SleepMonitor(connectionString);
+        }
+    }
+}
--- SE450 Sleep Tracker/Models/ExerciseIntensityModel.cs
+++ SE450 Sleep Tracker/Models/ExerciseIntensityModel.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SE450Database;
 
 namespace SE450_Sleep_Tracker.Models
 {
     public class ExerciseIntensityModel
     {
+        public int ID { get; set; }
+
+        public string Intensity { get; set; }
+
+        public ExerciseIntensityModel() { }
+
+        public ExerciseIntensityModel(Exi_ExerciseIntensity log)
+        {
+            ID = log.Exi_id;
+            Intensity = log.Exi_intensity;
+        }
+
+        public Exi_ExerciseIntensity ToDbObject()
+        {
+            return new Exi_ExerciseIntensity
+            {
+                Exi_id = ID,
+                Exi_intensity = Intensity
+            };
+        }
     }
 }
