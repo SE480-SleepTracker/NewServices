0e4d5b6fd6dc89b35603cfc04786cf0a7c98daab
ethansudman@comcast.net <ethansudman@comcast.net>
2014-11-10 00:25:17 -0600

    Initial commit

---
+++ SE450 Sleep Tracker/Models/NighttimeAwakeningsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SE450_Sleep_Tracker.Models
+{
+    public class NighttimeAwakeningsModel
+    {
+        public int ID { get; set; }
+        public SleepLogModel SleepLog { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
---
+++ SE450 Sleep Tracker/Models/PredefinedEmotionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SE450_Sleep_Tracker.Models
+{
+    public class PredefinedEmotionModel
+    {
+        public int ID { get; set; }
+
+        public string Emotion { get; set; }
+    }
+}
