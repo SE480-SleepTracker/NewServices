8861cfec091533d12fc5ac94719e22dafcddecb8
ethansudman@comcast.net <ethansudman@comcast.net>
2014-11-10 00:26:36 -0600

    Initial commit - context

---
+++ SE450 Sleep Tracker/ManualContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using SE450_Sleep_Tracker.Models;
+
+namespace SE450_Sleep_Tracker
+{
+    public class ManualContext : DbContext
+    {
+        public ManualContext() : base("name=ContextConnection") { }
+
+        public DbSet<EmotionLogModel> EmotionLogs { get; set; }
+
+        public DbSet<ExerciseLogModel> ExerciseLogs { get; set; }
+    }
+}
