d825148db7790a60055eded60deb0077ba3f7626
ethansudman@comcast.net <ethansudman@comcast.net>
2014-11-11 00:10:49 -0600

    Registration changes

--- SE450 Sleep Tracker/Startup.cs
+++ SE450 Sleep Tracker/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
 
 [assembly: OwinStartup(typeof(SE450_Sleep_Tracker.Startup))]
 
@@ -13,6 +15,12 @@ namespace SE450_Sleep_Tracker
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+
         }
     }
 }
