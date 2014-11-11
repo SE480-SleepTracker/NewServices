e2622639d6515397d6ab19d5bede23aff4809c7a
ethansudman@comcast.net <ethansudman@comcast.net>
2014-11-11 00:10:19 -0600

    EF chan

--- SE450Database/Framework.Designer.cs
+++ SE450Database/Framework.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'e:\My Documents\Visual Studio 2013\Projects\SE450 Sleep Tracker\SE450Database\Framework.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\My Documents\Visual Studio 2013\Projects\SE450 Sleep Tracker\SE450Database\Framework.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
