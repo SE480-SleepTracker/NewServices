40ccf0358f8c8e6b4e91ebbb6fbdd4d5179b7317
ethansudman@comcast.net <ethansudman@comcast.net>
2014-11-15 19:53:48 -0600

    Add new files

--- SE450 Sleep Tracker/App_Start/Startup.cs
+++ SE450 Sleep Tracker/App_Start/Startup.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
-using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
 
 [assembly: OwinStartup(typeof(SE450_Sleep_Tracker.Startup))]
 
@@ -15,12 +13,6 @@ namespace SE450_Sleep_Tracker
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-
-            var config = new HttpConfiguration();
-            WebApiConfig.Register(config);
-            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
-            app.UseWebApi(config);
-
         }
     }
 }
